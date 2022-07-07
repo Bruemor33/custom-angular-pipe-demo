f7b034f22da9f1b72cc40c121508bd63566eb3b2
Bruemor <arymer1@gmail.com>
2022-07-06 22:47:01 -0400

    Removed console log

--- src/app/replace-null-with-string.pipe.ts
+++ src/app/replace-null-with-string.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ReplaceNullWithStringPipe implements PipeTransform {
 
   transform(value: any, replacement: string = 'N/A'): any {
-    console.log(value)
+    
     if (typeof value === undefined || value === null) {
       return replacement
     }
